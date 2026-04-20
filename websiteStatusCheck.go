332474e498279c26e562763ccd01af2a9d1f8967
Matheus Victor Martins <matheusvictor.profissional@gmail.com>
2026-04-20 16:26:25 -0300

    Repositorio inicializado e sistema de monitoramento quase concluido

---
+++ websiteStatusCheck.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mostraIntroducao()
+	for {
+		mostraMenu()
+		comando := leComando()
+		executaComando(comando)
+	}
+
+}
+
+func mostraIntroducao() {
+	nome := "Matheus"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func mostraMenu() {
+	fmt.Println("\n1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair")
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando)
+	return comando
+}
+
+func executaComando(valor int) {
+	switch valor {
+	case 0:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo logs...")
+	default:
+		fmt.Println("Não reconheço esse comando.")
+		os.Exit(-1)
+	}
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Iniciando monitoramento...")
+
+	site := "https://www.alura.com.br"
+	requisicao, _ := http.Get(site)
+	if requisicao.StatusCode == 200 {
+		fmt.Println("Site:", site, "Foi carregado com sucesso!\nCódigo:", requisicao.StatusCode)
+	} else {
+		fmt.Println("Site:", site, "Possuí problemas.\nCódigo:", requisicao.StatusCode)
+	}
+
+}
